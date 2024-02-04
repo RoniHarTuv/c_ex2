80315a58fc64ec87ba6d8b16f022a9abdc8c8fb4
RoniHarTuv <149065885+RoniHarTuv@users.noreply.github.com>
2024-02-04 13:01:00 +0200

    Delete my_mat.h

--- my_mat.h
+++
@@ -1,8 +0,0 @@
-#define true 1
-#define false 0
-#define N 10
-
-void f_1(int mat[][N]);
-void f_2(int i, int j, int matrix[][N]);
-int f_3(int i, int j, int matrix[][N]);
-void builder(int mat[][N]);
